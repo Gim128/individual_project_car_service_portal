ce6b43464fe49c957115dea89c1766cc269e5d63
Gim128 <gimhanpabasara4@gmail.com>
2025-03-18 22:25:14 +0530

    booking service exception handle

--- src/main/java/com/example/carserviceportal/exception/CarNotFoundException.java
+++ src/main/java/com/example/carserviceportal/exception/CarNotFoundException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class CarNotFoundException extends RuntimeException {
-    public CarNotFoundException(int message) {
+    public CarNotFoundException(String message) {
         super(String.valueOf(message));
     }
 }
